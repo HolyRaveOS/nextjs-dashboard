b0912679901953900751eae90a2954a82656092f
HolyRaveOS <kitlaruk1995@gmail.com>
2023-12-05 17:40:43 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+export const lusitana = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-lusitana',
+});
